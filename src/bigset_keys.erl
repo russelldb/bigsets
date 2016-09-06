2c272bc36f05ad88bb0e58d40a9e1b5a6011c10b
Russell Brown <russell.brown@mac.com>
2016-09-06 15:07:08 +0100

    WIP: keys without comparator

--- src/bigset_keys.erl
+++ src/bigset_keys.erl
@@ -10,3 +10,42 @@
 -include("bigset.hrl").
 
 -compile([export_all]).
+
+-define(NULL, $\0).
+
+prefix(Set) ->
+    <<1, %%Magic byte
+      1:8/big-unsigned-integer, %% version
+      Set/binary, %% Set
+      ?NULL
+      >>.
+
+clock_key(Set, Actor) ->
+    <<(prefix(Set))/binary,
+      $c,
+      ?NULL, %% element field is null
+      Actor/binary,
+      ?NULL %% actor terminator
+    >>.
+
+tombstone_key(Set, Actor) ->
+    <<(prefix(Set))/binary,
+      $d,
+      ?NULL, %% element field is null
+      Actor/binary,
+      ?NULL>>.
+
+insert_member_key(Set, Element, Actor, Cnt) ->
+    <<(prefix(Set))/binary,
+      $e,
+      Element/binary,
+      ?NULL,
+      Actor/binary,
+      ?NULL,
+      Cnt:64/big-unsigned-integer>>.
+
+end_key(Set) ->
+    <<(prefix(Set))/binary,
+      $z,
+      ?NULL,
+      ?NULL>>.
